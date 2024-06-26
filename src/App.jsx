eb5b763a8ee695c50ecc1f74187b39ca2362c723
Kaichun <i1am2boss3@gmail.com>
2024-06-26 18:23:17 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home";
+import Services from "./components/Services/Services";
+import Banner from "./components/Banner/Banner";
+import Footer from "./components/Footer/Footer";
+
+export const App = () => {
+  useEffect(() => {
+    AOS.init({
+      offset: 200,
+      duration: 600,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+  });
+
+  return (
+    <div className="overflow-x-hidden">
+      <Navbar />
+      <Home />
+      <Services />
+      <Banner />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+        cursive: ["Indie Flower", "cursive"],
+      },
+      colors: {
+        primary: "#0080FF",
+        secondary: "#ADD8E6",
+        brandDark: "#f5f5dc",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
