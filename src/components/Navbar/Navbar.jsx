623277e64f07ee2865e158eb5aac4034c82f7f05
Kaichun <i1am2boss3@gmail.com>
2024-07-08 13:31:35 +0800

    add a test route

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
               className="font-bold text-2xl sm:text-3xl flex justify-center items-center gap-2 tracking-wider font-cursive"
             >
               <img src={Logo} alt="Logo" className="w-14" />
-              NoBody about Cafe
+              NoBody about Cafe 123
             </a>
           </div>
           {/* Links section*/}
