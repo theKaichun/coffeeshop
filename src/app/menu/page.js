25538ade3df11068aabbdb0acc9d4be2945c0b39
Kaichun <i1am2boss3@gmail.com>
2024-07-17 14:35:37 +0800

    add page

--- src/app/menu/page.js
+++ src/app/menu/page.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Logo from "@/assets/website/NBaC.png";
 import Image from "next/image";
+
 const menuItems = [
   {
     category: "髒咖啡 Dirty Coffee - 快速、好喝、不用等",
-    note: "(Expresso & Flat wnite)(棉花糖烤餅乾)(內用限定)",
+    note: "(Expresso & Flat white)(棉花糖烤餅乾)(內用限定)",
     items: [
       { name: "髒(黑)咖啡 Dirty Coffee", price: "65$" },
       { name: "髒拿鐵 Dirty Au lait", price: "80$" },
@@ -14,13 +15,10 @@ const menuItems = [
   {
     category: "現淬咖啡 Fresh coffee - 現點現做 3 min",
     items: [
-      { name: "現淬黑咖啡 Freah Black coffee", price: "80$" },
+      { name: "現淬黑咖啡 Fresh Black coffee", price: "80$" },
       { name: "現淬拿鐵 Fresh Au lait", price: "100$" },
       { name: "現淬小白 Fresh Flat white", price: "90$" },
-      {
-        name: "現淬壹加壹 1+1",
-        price: "120$",
-      },
+      { name: "現淬壹加壹 1+1", price: "120$" },
     ],
   },
   {
@@ -29,10 +27,7 @@ const menuItems = [
       { name: "單品咖啡 Single Origin Coffee" },
       { name: "單品拿鐵 Single Origin Au lait", price: "120$" },
       { name: "單品小白 Single Origin Flat white", price: "100$" },
-      {
-        name: "單品壹加壹 Single Origin 1+1",
-        price: "140$",
-      },
+      { name: "單品壹加壹 Single Origin 1+1", price: "140$" },
     ],
   },
   {
@@ -56,8 +51,8 @@ const menuItems = [
     category: "小東西 Snack",
     items: [
       { name: "棉花糖烤餅乾", price: "60$" },
-      { name: "起司雞蛋燒", price: "100~120$" },
-      { name: "肉桂捲", price: "原味、楓糖奶油、海鹽起司" },
+      { name: "起司雞蛋燒", price: "60$" },
+      { name: "肉桂捲(普通、楓糖奶油、海鹽起司)", price: "100~120$" },
       { name: "每日甜點", note: "→ 見燈箱" },
     ],
   },
@@ -65,27 +60,22 @@ const menuItems = [
 
 const Menu = () => {
   return (
-    <div className="menu bg-white px-10 ">
+    <div className="menu bg-white px-10">
       <Image
         src={Logo}
         alt="Logo"
-        className="w-[100px] sm:w-[200px] sm:scale-100 mx-auto mr-0 py-5   -skew-y-12 "
+        className="w-[100px] sm:w-[200px] sm:scale-100 mx-auto mr-0 py-5 -skew-y-12"
       />
       {menuItems.map((category, index) => (
         <div key={index}>
           <h2 className="text-1xl font-semibold flex items-center justify-center underline font-cursive">
             {category.category}
-            {category.note && (
-              <span className="text-sm text-gray-500 mr-2 ">
-                {category.note}
-              </span>
-            )}
           </h2>
           <ul className="mt-2">
             {category.items.map((item, itemIndex) => (
               <li
                 key={itemIndex}
-                className="flex justify-between py-1 text-blue-700 font-cursive font-bold text-3xl "
+                className="flex justify-between py-1 text-blue-700 font-cursive font-bold text-2xl"
               >
                 <span>{item.name}</span>
                 <span>{item.price}</span>
@@ -99,6 +89,16 @@ const Menu = () => {
           </ul>
         </div>
       ))}
+      <div className="mt-10 text-center">
+        {menuItems.map(
+          (category, index) =>
+            category.note && (
+              <div key={index} className="text-sm text-gray-500 my-2">
+                {category.note}
+              </div>
+            )
+        )}
+      </div>
     </div>
   );
 };
