cb2507e2eea7bca0d511207a2c7bb984ea2d1e3d
Kaichun <i1am2boss3@gmail.com>
2024-07-17 17:36:21 +0800

    add mr

--- src/app/menu/page.js
+++ src/app/menu/page.js
@@ -161,20 +161,20 @@ const MenuList = ({ items }) => {
                   </span>
                 )}
                 <div className="flex text-black text-sm font-cursive text-nowrap ">
-                  {item.roast && <span className="mr-10">{item.roast}</span>}
+                  {item.roast && <span className="mr-5">{item.roast}</span>}
 
                   {item.processing && (
-                    <span className="mr-10">{item.processing}</span>
+                    <span className="mr-5">{item.processing}</span>
                   )}
                   {item.production && (
-                    <span className="mr-10">{item.production}</span>
+                    <span className="mr-5">{item.production}</span>
                   )}
                   {item.varieties && (
-                    <span className="mr-10">{item.varieties}</span>
+                    <span className="mr-5">{item.varieties}</span>
                   )}
                 </div>
                 <div className="flex text-black text-sm font-cursive ">
-                  {item.flavor && <span className="mr-10">{item.flavor}</span>}
+                  {item.flavor && <span className="mr-5">{item.flavor}</span>}
                 </div>
               </li>
             ))}
